5327eb3e93987ec265b973db541806ddb8cab906
ceallan.shen@gmail.com <ceallan.shen@gmail.com@f6073aa7-924d-aa36-7430-ef82b93690e0>
2011-01-05 12:02:23 +0000

    url规则

--- protected/config/main.php
+++ protected/config/main.php
@@ -35,16 +35,18 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>/*'=>'<controller>/<action>',
 			),
+            'showScriptName'=>false,
+            'urlSuffix'=>'.htm'
 		),
-		*/
+
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
