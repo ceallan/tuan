599ab12087bd303e9264c8e49ba66487b7a6aae8
ceallan.shen@gmail.com <ceallan.shen@gmail.com@f6073aa7-924d-aa36-7430-ef82b93690e0>
2011-01-05 13:34:20 +0000

    url规则

--- protected/config/main.php
+++ protected/config/main.php
@@ -41,7 +41,7 @@ return array(
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>/*'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
             'showScriptName'=>false,
             'urlSuffix'=>'.html'
